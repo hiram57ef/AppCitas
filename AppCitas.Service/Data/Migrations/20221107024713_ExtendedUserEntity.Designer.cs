52c7ffc02776c02241f00d2eff2828f47d8af754
hiram57ef <orociohiram@gmail.com>
2022-11-06 20:50:54 -0600

    Entity Framework relationships

--- AppCitas.Service/Data/Migrations/20221107024713_ExtendedUserEntity.Designer.cs
+++ AppCitas.Service/Data/Migrations/20221107024713_ExtendedUserEntity.Designer.cs
@@ -3,16 +3,18 @@ using System;
 using AppCitas.Service.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
 
-namespace AppCitas.Service.Data.Migrations
+namespace AppCitas.Service.data.Migrations
 {
     [DbContext(typeof(DataContext))]
-    partial class DataContextModelSnapshot : ModelSnapshot
+    [Migration("20221107024713_ExtendedUserEntity")]
+    partial class ExtendedUserEntity
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder.HasAnnotation("ProductVersion", "6.0.8");
